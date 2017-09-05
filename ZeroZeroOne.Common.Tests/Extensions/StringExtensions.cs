ff2b4a0e079cc31bb1cddc3bb21890fbea09856d
William Hurst <william@001.co.za>
2017-09-05 20:28:30 +0200

    Added StringExtensions.HasValue method

---
+++ ZeroZeroOne.Common.Tests/Extensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using ZeroZeroOne.Common.Extensions;
+
+namespace ZeroZeroOne.Common.Tests.Extensions
+{
+    [TestFixture()]
+    public class StringExtensionsTests
+    {
+        [Test()]
+        public void HasValue_Returns_True_If_String_Is_Not_Empty()
+        {
+            var theValue = "This is a string";
+
+            var hasValue = theValue.HasValue();
+
+            Assert.IsTrue(hasValue);
+        }
+
+		[Test()]
+		public void HasValue_Returns_False_If_String_Is_Empty()
+		{
+			var theValue = "";
+
+			var hasValue = theValue.HasValue();
+
+			Assert.IsFalse(hasValue);
+		}
+
+		[Test()]
+		public void HasValue_Returns_False_If_String_Is_Null()
+		{
+			String theValue = null;
+
+			var hasValue = theValue.HasValue();
+
+			Assert.IsFalse(hasValue);
+		}
+    }
+}
---
+++ ZeroZeroOne.Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("ZeroZeroOne.Common")]
+[assembly: AssemblyDescription("Zero Zero One Common Functions Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Zero Zero One")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("0.1.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
