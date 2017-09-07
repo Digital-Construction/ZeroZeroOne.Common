25f24568e999f57b6688be43c813da4275146c6d
William Hurst <william@001.co.za>
2017-09-07 07:24:39 +0200

    Add ToYesNo and ToTrueFalseText methods in Boolean Extensions Clean StringExtension tests based on R# Added missing method comments to StringExtensions

---
+++ ZeroZeroOne.Common.Tests/Extensions/BooleanExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using ZeroZeroOne.Common.Extensions;
+
+namespace ZeroZeroOne.Common.Tests.Extensions
+{
+    [TestFixture]
+    public class BooleanExtensionsTests
+    {
+        [Test]
+        public void ToYesNo_Returns_Yes_If_Value_Is_True()
+        {
+            Assert.AreEqual("Yes", true.ToYesNo());
+        }
+
+        [Test]
+		public void ToYesNo_Returns_No_If_Value_Is_False()
+		{
+		    Assert.AreEqual("No", false.ToYesNo());
+		}
+        
+        [Test]
+        public void ToTrueFalseText_Returns_TrueString_If_Value_Is_True()
+        {
+            Assert.AreEqual("Ab!1234", true.ToTrueFalseText("Ab!1234", "False"));
+        }
+
+        [Test]
+        public void ToTrueFalseText_Returns_FalseString_If_Value_Is_False()
+        {
+            Assert.AreEqual("Yz!1234", false.ToTrueFalseText("True", "Yz!1234"));
+        }
+    }
+}
---
+++ ZeroZeroOne.Common/Extensions/BooleanExtensions.cs
@@ -0,0 +1,44 @@
+﻿//
+//  Copyright 2017  Zero Zero One
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+using System;
+namespace ZeroZeroOne.Common.Extensions
+{
+    public static class BooleanExtensions
+    {
+        /// <summary>
+        /// Converts a boolean value to a string of Yes or No
+        /// </summary>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        /// <returns></returns>
+        public static String ToYesNo(this Boolean value)
+        {
+            return value.ToTrueFalseText("Yes", "No");
+        }
+
+        /// <summary>
+        /// Converts a boolean value to a specified string for the true or false value
+        /// </summary>
+        /// <param name="value">if set to <c>true</c> [value].</param>
+        /// <param name="trueText">The true text.</param>
+        /// <param name="falseText">The false text.</param>
+        /// <returns></returns>
+        public static String ToTrueFalseText(this Boolean value, String trueText, String falseText)
+        {
+            return value ? trueText : falseText;
+        }
+
+    }
+}
